c35304ddc96ec4ebebad6c5c035ea9d4836ebe18
Chandima Ranaweera <chan4lk@gmail.com>
2019-01-06 18:26:54 +0530

    Local notification added.

--- App.js
+++ App.js
@@ -7,8 +7,8 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import { showNotification, configure } from "./Notifications";
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -18,12 +18,19 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  componentDidMount(){
+    configure();
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Button title="Show" onPress={() => {          
+          showNotification();
+        }
+        }/>
       </View>
     );
   }
